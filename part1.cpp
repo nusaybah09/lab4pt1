0da85f89d8738b5c57ca10871afc269adb814ec8
nusaybah <nusaybahahmed09@gmail.com>
2025-10-21 11:47:56 -0400

    Add files via upload

---
+++ part1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+struct CountryPopulation {
+	int number;	//number of people
+	char value; //to represent billons or millions
+	string name; // name of country
+};
+
+int main() {
+	string countryName;
+
+	unordered_map<string, CountryPopulation> data;
+
+	data["America"] = CountryPopulation{ 340, 'M' };
+	data["Europe"] = CountryPopulation{ 744, 'M' };
+	data["Brazil"] = CountryPopulation{ 213,'M' };
+	data["Mexico"] = CountryPopulation{ 132, 'M' };
+	data["Egypt"] = CountryPopulation{ 118, 'M' };
+
+	//display all values
+
+	for (auto& [name, countryInfo] : data) {
+		cout << "Country Name: " << name << endl;
+		cout << "Population: " << countryInfo.number << " " << countryInfo.value << endl;
+	}
+
+	//searching for a country
+	cout << "Enter the country you want to search: " << endl;
+	getline(cin, countryName);
+
+	auto foundCountry = data.find(countryName);
+
+	if (foundCountry != data.end()) {
+		cout << "Country was found!" << endl;
+		cout << "Country Name: " << foundCountry->first << endl;
+		cout << "Population: " << foundCountry->second.number << " " << foundCountry->second.value << endl;
+	}
+	else{
+		cout << "Country was not found! " << endl;
+	}
+		
+	//finding canada
+	if (data.find("Canada") != data.end()) {
+		cout << "Canada was not found!" << endl;
+	}
+
+
+}
